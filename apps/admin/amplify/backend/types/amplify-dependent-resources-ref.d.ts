5f29c3a0e21ba867977e8f61efe315872a844413
Baixm <baixm92@163.com>
2024-05-04 13:57:36 +0800

    init

--- apps/admin/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ apps/admin/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "basebackend": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
